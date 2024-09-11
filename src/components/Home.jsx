c0cf61370bc4bc915519bf8d03cc6edddb641ab6
devoloper mickey <arpanbera475@gmail.com>
2024-09-11 19:17:03 +0530

    pic changed!

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import pic from "../../public/photo.avif";
+import pic from "../../public/pic.jpg";
 import { FaSquareFacebook } from "react-icons/fa6";
 import { FaLinkedin } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
